f846bddeb0b2fbee1f3acd21aa5874ec59e551ff
bufgix <ooruc471@yandex.com>
2020-03-18 18:15:22 +0300

    Title more dynamic

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Head from "next/head";
+import config from "../config";
 
 function App({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>Bufgix Blog</title>
+        <title>{`${config.username} Blog`}</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
